da32362d40a4ac2e5e2efb8fd1979ff2283fa8f1
Rysanek <rysanekrivera@gmail.com>
2021-04-23 16:45:25 -0400

    Started File Selection Dialog, Play Fragment Layout continuation

--- app/src/main/java/com/rysanek/soundsapp/ui/fragments/PlayFragment.kt
+++ app/src/main/java/com/rysanek/soundsapp/ui/fragments/PlayFragment.kt
@@ -1,10 +1,16 @@
 package com.rysanek.soundsapp.ui.fragments
 
+import android.app.AlertDialog
+import android.app.Dialog
 import android.os.Bundle
+import android.os.storage.StorageManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat
+import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
+import com.google.android.material.button.MaterialButton
 import com.rysanek.soundsapp.R
 import com.rysanek.soundsapp.databinding.FragmentPlayBinding
 
@@ -12,6 +18,7 @@ class PlayFragment: Fragment() {
     
     private var _binding: FragmentPlayBinding? = null
     private val binding get() = _binding!!
+    private val selected = mutableListOf<MaterialButton>()
     
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,6 +27,35 @@ class PlayFragment: Fragment() {
     ): View {
         
         _binding = FragmentPlayBinding.inflate(inflater, container, false)
+        
+        listOf(binding.btn1, binding.btn2, binding.btn3, binding.btn4, binding.btn5, binding.btn6, binding.btn7, binding.btn8, binding.btn9).onEach { button ->
+            
+            button.setOnClickListener {
+                if (!selected.isNullOrEmpty()) {
+                    if (!selected.contains(button)) {
+                        selected[0].setTextColor(resources.getColor(R.color.white, resources.newTheme()))
+                        selected.clear()
+                    }
+                }
+                selected.add(button)
+                button.setTextColor(resources.getColor(R.color.yellow, resources.newTheme()))
+            }
+            
+            button.setOnLongClickListener {
+                val builder = AlertDialog.Builder(it.context)
+                builder.setMessage("Select Sound File")
+                    .setView(R.layout.file_dialog)
+                    .setPositiveButton("Done") { dialog, id ->
+            
+                    }
+                    .setNegativeButton("Cancel") { dialog, id ->
+            
+                    }
+    
+                builder.create().show()
+                true
+            }
+        }
         
         return binding.root
     }
